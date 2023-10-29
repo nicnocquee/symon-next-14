b6db1ed52f4248b5746b6adf6cc87a4879939a8c
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-10-29 20:26:02 +0100

    add locations seed

--- src/app/api/seed/locations/route.ts
+++ src/app/api/seed/locations/route.ts
@@ -1,20 +1,34 @@
-import { generateHash } from '@/lib/password';
 import { prismaClient } from '@/prisma/prisma-client';
 import { faker } from '@faker-js/faker';
+import z from 'zod';
 import { NextResponse } from 'next/server';
+import { notFound } from 'next/navigation';
 
-export async function POST(_request: Request) {
-  const password = `abcd`;
-  console.log(password);
-  const user = await prismaClient.user.create({
-    data: {
-      name: faker.person.fullName(),
-      email: faker.internet.email().toLowerCase(),
-      password: await generateHash(password),
-      isEnabled: 1,
-      isSuspended: 0
-    }
-  });
+export async function POST(request: Request) {
+  const formData = (await request.json()) || {};
+  const validated = z
+    .object({
+      count: z.coerce.number().min(1).default(5)
+    })
+    .safeParse(formData || {});
 
-  return NextResponse.json({ result: user });
+  if (!validated.success) {
+    notFound();
+  }
+
+  const { count } = validated.data;
+  const locations = await Promise.all(
+    Array.from(Array(count).keys()).map((i) => {
+      return prismaClient.location.create({
+        data: {
+          city: faker.location.city(),
+          country: faker.location.country(),
+          isp: faker.company.name(),
+          code: faker.string.alphanumeric(3)
+        }
+      });
+    })
+  );
+
+  return NextResponse.json({ result: locations });
 }
