58538006ac2e66d0d147481dbbbf7d737c1a16f3
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-10-30 13:33:50 +0100

    add probe page loading

---
+++ src/app/dashboard/probe/[nanoid]/loading.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+const ProbePageLoading = () => {
+  return (
+    <div className="w-full p-4 space-y-2">
+      <Skeleton className="h-20 w-[250px]" />
+      <Skeleton className="h-4 w-[200px]" />
+    </div>
+  );
+};
+
+export default ProbePageLoading;
