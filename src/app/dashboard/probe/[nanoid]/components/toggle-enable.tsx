07f711d9f1f61e3ef5afcaace6abfb4ec6e3fcd9
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-10-31 13:05:43 +0100

    set optimistic

--- src/app/dashboard/probe/[nanoid]/components/toggle-enable.tsx
+++ src/app/dashboard/probe/[nanoid]/components/toggle-enable.tsx
@@ -1,12 +1,26 @@
+'use client';
+
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { getProbeType, toggleProbe } from '@/usecases/probes';
+import { useOptimistic } from 'react';
 
 export function ToggleEnable({ probe }: { probe: NonNullable<getProbeType> }) {
+  const [probeEnabled, setProbeEnabled] = useOptimistic(
+    probe.isEnabled,
+    (current, _optimVal) => {
+      return !current;
+    }
+  );
   return (
     <div>
-      <form action={toggleProbe} className="flex items-center space-x-2">
-        <Switch id="probe-enabled" type="submit" checked={probe.isEnabled} />
+      <form
+        action={async (formData: FormData) => {
+          setProbeEnabled(!probeEnabled);
+          await toggleProbe(formData);
+        }}
+        className="flex items-center space-x-2">
+        <Switch id="probe-enabled" type="submit" checked={probeEnabled} />
         <input type="hidden" value={probe.id} name="probeId" />
         <Label htmlFor="probe-enabled">
           {probe.isEnabled ? 'Active' : 'Disabled'}
