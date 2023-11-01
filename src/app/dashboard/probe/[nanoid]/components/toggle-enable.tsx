8ba088ea345534d92c03db82fd908ed903edf47c
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-11-01 22:15:26 +0100

    disable toggle on submit

--- src/app/dashboard/probe/[nanoid]/components/toggle-enable.tsx
+++ src/app/dashboard/probe/[nanoid]/components/toggle-enable.tsx
@@ -4,6 +4,7 @@ import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 import { getProbeType, toggleProbe } from '@/usecases/probes';
 import { startTransition, useOptimistic } from 'react';
+import { useFormStatus } from 'react-dom';
 
 export function ToggleEnable({ probe }: { probe: NonNullable<getProbeType> }) {
   const [probeEnabled, setProbeEnabled] = useOptimistic(
@@ -20,20 +21,10 @@ export function ToggleEnable({ probe }: { probe: NonNullable<getProbeType> }) {
 
   return (
     <div>
-      <form
-        action={async (formData: FormData) => {
-          await toggleProbe(formData);
-        }}
-        className="flex items-center space-x-2">
-        <Switch
-          id="probe-enabled"
-          type="submit"
-          checked={probeEnabled}
-          onCheckedChange={(checked) => {
-            startTransition(() => {
-              setProbeEnabled(checked);
-            });
-          }}
+      <form action={toggleProbe} className="flex items-center space-x-2">
+        <EnableToggle
+          probeEnabled={probeEnabled}
+          setProbeEnabled={setProbeEnabled}
         />
         <input type="hidden" value={probeEnabled ? 1 : 0} name="isEnabled" />
         <input type="hidden" value={probe.id} name="probeId" />
@@ -44,6 +35,30 @@ export function ToggleEnable({ probe }: { probe: NonNullable<getProbeType> }) {
     </div>
   );
 }
+
+const EnableToggle = ({
+  probeEnabled,
+  setProbeEnabled
+}: {
+  probeEnabled: boolean;
+  setProbeEnabled: (action: boolean) => void;
+}) => {
+  const { pending } = useFormStatus();
+  console.log(pending);
+  return (
+    <Switch
+      disabled={pending}
+      id="probe-enabled"
+      type="submit"
+      checked={probeEnabled}
+      onCheckedChange={(checked) => {
+        startTransition(() => {
+          setProbeEnabled(checked);
+        });
+      }}
+    />
+  );
+};
 
 /*
 export function ToggleEnable({ probe }: { probe: NonNullable<getProbeType> }) {
