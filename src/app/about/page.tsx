11aa30ceea5df3aeea1a8dcda66c4bffeccaaa16
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-11-02 15:28:57 +0100

    tweak about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,24 +1,25 @@
 import { getLoggedInUser } from '@/usecases/user';
 import { getNanoId, getServerTime } from './time-server';
 import { revalidateTag } from 'next/cache';
+import { Button } from '@/components/ui/button';
 
 const AboutPage = async () => {
   const user = await getLoggedInUser();
   const content = await getServerTime(user?.id || 'anon');
   const nanoid = await getNanoId();
   return (
-    <div>
+    <div className="p-4 flex flex-col space-y-2">
       <h1>About page</h1>
       <p>{content}</p>
       <p>{nanoid}</p>
       <form
+        id="refreshform"
         action={async () => {
           'use server';
           revalidateTag('get-nano-id');
           revalidateTag('get-server-time');
-          revalidateTag('user-probes');
         }}>
-        <button>Refresh</button>
+        <Button form="refreshform">Refresh</Button>
       </form>
     </div>
   );
