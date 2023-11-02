02b91bbddf257a5cd6dfce07005adf939e9277a9
Nico Prananta <nicnocquee@users.noreply.github.com>
2023-11-02 16:53:31 +0100

    return error component from server action

--- src/app/search/search-action.tsx
+++ src/app/search/search-action.tsx
@@ -1,4 +1,7 @@
-import { randomBool, sleepRandom } from '@/lib/utils';
+// import { AlertCircle } from 'lucide-react';
+
+// import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
+// import { randomBool, sleepRandom } from '@/lib/utils';
 import { faker } from '@faker-js/faker';
 import { SearchResult } from './search-result';
 
@@ -6,11 +9,20 @@ export const search = async (keyword: string) => {
   'use server';
 
   console.log(keyword);
-  await sleepRandom();
+  // await sleepRandom();
 
-  if (!randomBool()) {
-    throw new Error('some error');
-  }
+  //   if (!randomBool()) {
+  //     // throw new Error('some error'); // throw error that will be picked up by ErrorBoundary
+  //     return (
+  //       <Alert variant="destructive">
+  //         <AlertCircle className="h-4 w-4" />
+  //         <AlertTitle>Error</AlertTitle>
+  //         <AlertDescription>
+  //           Cannot search the {keyword}. Please try again later.
+  //         </AlertDescription>
+  //       </Alert>
+  //     );
+  //   }
 
   // search definition here via API or something
 
